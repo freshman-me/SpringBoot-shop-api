f24de406f7f999f04465623fe7e0a8e245174faf
freshman <2757240832@qq.com>
2022-11-07 22:07:29 +0800

    add create token

---
+++ src/main/java/com/freshman/jwt/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.freshman.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.freshman.pojo.User;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+
+    // 密钥
+    public static final String SECRET = "secret";
+
+    /** token 过期时间: 10天 */
+    public static final int calendarField = Calendar.DATE;
+    public static final int calendarInterval = 10;
+
+    /**
+     *  创建token
+     * @param user 用户的信息
+     * @return  token
+     */
+   public static String createToken(User user){
+       // 现在时间
+       Date iatDate = new Date();
+
+       // 过期时间
+       Calendar nowTime = Calendar.getInstance();
+       nowTime.add(calendarField, calendarInterval);
+       Date expiresDate = nowTime.getTime();
+
+       // header Map
+       Map<String, Object> map = new HashMap<>();
+       map.put("alg", "HS256");
+       map.put("typ", "JWT");
+
+       String token = JWT.create().withHeader(map)  // header
+               .withClaim("id",user.getId().toString())
+               .withClaim("username",user.getUsername())
+               .withClaim("isAdmin",user.getIsAdmin().toString())  // payload
+               .withIssuedAt(iatDate)  // sign date
+               .withExpiresAt(expiresDate) // expire date
+               .sign(Algorithm.HMAC256(SECRET));  // signature
+
+        return token;
+   }
+}
